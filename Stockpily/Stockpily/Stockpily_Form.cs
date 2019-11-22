de9326de46f7eff66ec7fe4c29318c238d031432
Unknown <mihai.vlad.popa@philips.com>
2019-11-22 14:40:15 +0100

    First Commit. Project intialization

---
+++ Stockpily/Stockpily/Stockpily_Form.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stockpily
+{
+    public partial class Stockpily_Form : Form
+    {
+        public Stockpily_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
